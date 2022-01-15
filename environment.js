46e09ffd205fb6196eb0c88ea2d717a254852eab
Emilio Franceschini <franceschini.emilio@gmail.com>
2022-01-15 18:23:36 -0300

    https

--- environment.js
+++ environment.js
@@ -3,6 +3,8 @@ var environment = {};
 environment.EXPRESS_PORT = 3001;
 
 environment.APP_BASE_PATH = "/home/emoporemilio/";
+environment.certificate = "/etc/letsencrypt/live/emoporemilio.uy/fullchain.pem";
+environment.privateKey = "/etc/letsencrypt/live/emoporemilio.uy/privkey.pem"
 
 module.exports = environment;
 
